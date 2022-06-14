d63b20bfa1dd4ac4c0af47c36c3e5fcf014b03a3
Cristiano10 <ccristiandarioromero@gmail.com>
2022-06-14 14:02:33 -0300

    init

---
+++ src/main/java/com/abmlAPI/Portfolio/PortfolioApplication.java
@@ -0,0 +1,18 @@
+package com.abmlAPI.Portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PortfolioApplication {
+
+	public static void main(String[] args) {
+        
+                SpringApplication.run(PortfolioApplication.class, args);
+                  System.out.println("hiiii");
+           
+		
+	}
+
+}
