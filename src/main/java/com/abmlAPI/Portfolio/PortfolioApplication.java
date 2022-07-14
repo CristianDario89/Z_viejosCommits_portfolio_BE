f8c392ee507f26c924ad1e0b80d8cc7519c38cd8
digitalessomos <ccristiandarioromero@gmail.com>
2022-07-14 16:26:17 -0300

    ajuste02

--- src/main/java/com/abmlAPI/Portfolio/PortfolioApplication.java
+++ src/main/java/com/abmlAPI/Portfolio/PortfolioApplication.java
@@ -1,14 +1,7 @@
 package com.abmlAPI.Portfolio;
-
-import java.time.Duration;
-import java.util.Arrays;
+ 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
 
 @SpringBootApplication
 public class PortfolioApplication {
@@ -19,20 +12,6 @@ public class PortfolioApplication {
                   System.out.println("holaa funca todo broooo");
            
 		
-	}
-@Bean
- CorsConfigurationSource corsConfigurationSource() {
-  CorsConfiguration cc = new CorsConfiguration();
-                cc.setAllowedHeaders(Arrays.asList("Origin,Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers","Authorization"));
-                cc.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));                
-                cc.setAllowedOrigins(Arrays.asList("/*"));
-                cc.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT","PATCH"));
-                cc.addAllowedOrigin("*");
-                cc.setMaxAge(Duration.ZERO);
-                cc.setAllowCredentials(Boolean.TRUE);
-  UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-  source.registerCorsConfiguration("/**", cc);
-  return source;
- }
+	} 
 }
 
