d29c43fe23b3044f79f6ce0724bb16251746b2e1
digitalessomos <ccristiandarioromero@gmail.com>
2022-07-18 15:06:52 -0300

    this never ends 02

--- src/main/java/com/abmlAPI/Portfolio/controller/UserController.java
+++ src/main/java/com/abmlAPI/Portfolio/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
  
 @RestController 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api") 
 public class UserController {
     @Autowired
