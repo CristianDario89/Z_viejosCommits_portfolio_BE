a9e055e3602eb8156cdd7e48c45879dd3fc18a42
digitalessomos <ccristiandarioromero@gmail.com>
2022-07-21 14:37:34 -0300

    cors 01

--- src/main/java/com/abmlAPI/Portfolio/PortfolioApplication.java
+++ src/main/java/com/abmlAPI/Portfolio/PortfolioApplication.java
@@ -2,6 +2,9 @@ package com.abmlAPI.Portfolio;
  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer; 
 
 @SpringBootApplication
 public class PortfolioApplication {
@@ -11,12 +14,19 @@ public class PortfolioApplication {
                   System.out.println("holaa funca todo broooo");
          } 
         
+        @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
+        
 }
 
-/*import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;*/
-
+/*
  /*
       @Bean
 	public WebMvcConfigurer corsConfigurer() {
